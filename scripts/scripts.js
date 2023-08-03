90116d06fbed92aed0b8021f176a9f105592cd8a
bharanilogesh <119596538+bharanilogesh@users.noreply.github.com>
2023-08-03 11:48:06 +0530

    final

---
+++ scripts/scripts.js
@@ -0,0 +1,42 @@
+"use script";
+
+
+let daysClock = document.getElementById("dayBox");
+let hoursClock = document.getElementById("hoursBox");
+let minutesClock = document.getElementById("minutesBox");
+let secondsClock = document.getElementById("secondsBox");
+let msg = document.getElementById("msg");
+
+let day = 1;
+daysClock.innerHTML = day;
+let hours = 23;
+hoursClock.innerHTML = hours;
+let minutes = 59;
+minutes.innerHTML = minutes;
+let seconds = 59;
+secondsClock.innerHTML = seconds;
+
+setInterval(() => {
+  seconds--;
+  secondsClock.innerHTML = seconds;
+  if (seconds === 0) {
+    minutes--;
+    minutesClock.innerHTML = minutes;
+    seconds = 59;
+
+    if (minutes === 0) {
+      hours--;
+      hoursClock.innerHTML = hours;
+      minutes = 59;
+
+      if (hours === 0) {
+        daysClock.innerHTML = day--;
+        day = 0;
+      }
+    }
+
+    if (day === 0) {
+      msg.innerHTML = "You Offer Is Gone";
+    }
+  }
+}, 1000);
